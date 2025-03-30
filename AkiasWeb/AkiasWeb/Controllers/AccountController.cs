920b7c6897a83496c009f47447238dbf224a2b97
gaus4415 <gausmohd4415@gmail.com>
2025-03-30 20:08:44 +0530

    create project architecture

---
+++ AkiasWeb/AkiasWeb/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkiasWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {            
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+			return RedirectToAction("Index", "Home");
+
+		}
+	}
+}
---
+++ AkiasWeb/AkiasWeb/Controllers/TeacherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkiasWeb.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
